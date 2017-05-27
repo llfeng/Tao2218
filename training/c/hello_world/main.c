1a98504744f515191b25520995b47e5e37d83be7
llfeng <llfeng@ubuntu64.(none)>
2017-05-27 23:07:17 +0800

    add training directory

---
+++ training/c/hello_world/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ * Discription:
+ *
+ * Copyright (C) 2017-05-27 22:38 kevin.feng <flilei0917@163.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+	printf("hello world!\n");
+	return 0;
+}
---
+++ training/python/hello_world/hello_world.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 kevin.feng <flilei0917@163.com>
+#
+# Distributed under terms of the MIT license.
+
+def hello_world():
+	print('hello world!')
+
+if __name__ == '__main__':
+	hello_world()
+
+
